2ab87a973a1cd4ba2d50b22107cc344e142fc1dc
Joe Robles <joe@colldev.com>
2013-10-14 00:03:59 -0500

    entity submit button reverted

--- src/Petramas/MainBundle/Resources/public/js/scripts.js
+++ src/Petramas/MainBundle/Resources/public/js/scripts.js
@@ -1,14 +1,32 @@
 $(document).on('ready', function(){
     $("[rel='tooltip']").tooltip();
     
-    $('a.entity').on('click', function(e){
-        e.preventDefault();
-        $.ajax({
-            type: "GET",
-            url: $(this).attr('href'),
-            success: function(html) {
-                $('#petramasModal').html(html).modal();
-            }
-        });
-    });
+//    $('a.entity').on('click', function(e){
+//        e.preventDefault();
+//        $.ajax({
+//            type: "GET",
+//            url: $(this).attr('href')
+//        }).done(function(html){
+//            $('#petramasModal').html(html).modal();
+//        });
+//    });
+    
+//    $(document).on('click', 'button.entity', function(e){
+//        e.preventDefault();
+//        var $parent = $(this).parent().parent();
+//        $.ajax({
+//            type: "POST",
+//            url: $parent.attr('action'),
+//            data: $('.' + $parent.attr('class')).serialize()
+//        }).done(function(returnValue){
+//            if (returnValue.status !== 200) {
+//                $('#petramasModal').html(returnValue);
+//            }
+//        });
+//    });
+    
+//    $('a.delete').on('click', function(e){
+//        e.preventDefault();
+//        
+//    });
 });
