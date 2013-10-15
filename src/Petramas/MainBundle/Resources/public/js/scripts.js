b28ef04edb23b0ba2de9a321ff2e802e6671d736
Joe Robles <joe.robles.pdj@gmail.com>
2013-10-15 11:40:45 -0500

    confirm delete implemented

--- src/Petramas/MainBundle/Resources/public/js/scripts.js
+++ src/Petramas/MainBundle/Resources/public/js/scripts.js
@@ -1,3 +1,18 @@
+var $modal = '<div class="modal-dialog">\n\
+    <div class="modal-content">\n\
+      <div class="modal-header">\n\
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>\n\
+        <h4 class="modal-title">{{ title }}</h4>\n\
+      </div>\n\
+      <div class="modal-body">\n\
+        <p>{{ body }}</p>\n\
+      </div>\n\
+      <div class="modal-footer">\n\
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>\n\
+        <button type="button" class="btn btn-danger delete-remove">{{ ok }}</button>\n\
+      </div>\n\
+    </div>\n\
+  </div>';
 $(document).on('ready', function(){
     $("[rel='tooltip']").tooltip();
     
@@ -27,8 +42,18 @@ $(document).on('ready', function(){
     
     $('button.entity-submit-delete').on('click', function(e){
         e.preventDefault();
-        if (confirm('¿Desea eliminar el registro?')) {
-            $(this).parent().parent().parent().submit();
-        }
+        var $this = this;
+        var data = {
+            title: 'Eliminar registro',
+            body: '¿Esta seguro de elimnar el registro?',
+            ok: 'Eliminar'
+        };
+        modal = $.mustache($modal, data);
+        $('#petramasModal').html(modal).modal();
+
+        $(document).on('click', 'button.delete-remove', function(){
+            $($this).parent().parent().parent().submit();
+            $(this).parent().find('button').attr('data-dismiss', 'modal').trigger('click');
+        });
     });
 });
