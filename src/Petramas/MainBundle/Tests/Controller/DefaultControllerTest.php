54e5f7bda69d3c072d2b4795a5b14d7bd4f3be2e
Joe Robles <joe.robles.pdj@gmail.com>
2013-11-07 12:52:26 -0500

    tests prepared

--- src/Petramas/MainBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Petramas/MainBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
+    private function getHostUrl($hostId, $path = '')
+    {
+        return self::$kernel->getContainer()->getParameter('petramas.test.hostname.'.$hostId).$path;
+    }
+    
     public function testIndex()
     {
-        $client = static::createClient();
+        $client = static::createClient(array('test', true), array(
+            'PHP_AUTH_USER' => 'jose',
+            'PHP_AUTH_PW'   => 'jose',
+        ));
+        $crawler = $client->request('GET', $this->getHostUrl('local', '/'));
 
-        $crawler = $client->request('GET', '/');
-
-        $this->assertTrue($crawler->filter('html:contains("Hello!")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Bienvenido")')->count() > 0);
     }
 }
